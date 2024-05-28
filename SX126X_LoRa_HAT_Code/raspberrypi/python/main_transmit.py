05ce1f267b29a0880996cd4f8df0523989973ccd
psonnh <p.sonnhuetter17@gmail.com>
2024-05-28 13:16:34 +0200

    transmit file edited

--- SX126X_LoRa_HAT_Code/raspberrypi/python/main_transmit.py
+++ SX126X_LoRa_HAT_Code/raspberrypi/python/main_transmit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-import sys
+#    Need to disable the serial login shell and have to enable serial interface 
+#    command `sudo raspi-config`
+#    More details: see https://github.com/MithunHub/LoRa/blob/main/Basic%20Instruction.md
+#
+#    When the LoRaHAT is attached to RPi, the M0 and M1 jumpers of HAT should be removed.
+
 import sx126x
-import threading
 import time
-import select
-import termios
-import tty
 from threading import Timer
 import json
 from glob import glob
@@ -17,19 +18,6 @@ json_directory = '/home/test/Desktop/team-bms/battery_data_json'
 last_file = None
 seconds = 5
 
-
-
-
-#
-#    Need to disable the serial login shell and have to enable serial interface 
-#    command `sudo raspi-config`
-#    More details: see https://github.com/MithunHub/LoRa/blob/main/Basic%20Instruction.md
-#
-#    When the LoRaHAT is attached to RPi, the M0 and M1 jumpers of HAT should be removed.
-#
-
-
-# Funktion um die neueste JSON-Datei zu finden
 def get_latest_json_file():
     files = glob(os.path.join(json_directory, "battery_data_*.json"))
     if not files:
@@ -41,28 +29,19 @@ def get_latest_json_file():
     last_file = latest_file
     return latest_file
 
-#   Read an json file and send the data once to another node
-def send_json_file():
-    with open(get_latest_json_file(), 'r') as f:
-        data = json.load(f)
-
-    data = bytes([255]) + bytes([255]) + bytes([18]) + bytes([255]) + bytes([255]) + bytes([12]) + "Battery Data:".encode()+str(data).encode()
-    node.send(data)
-    time.sleep(0.2)
-
 def send_json_file_continue(continue_or_not = True):
     if continue_or_not:
         global timer_task
         global seconds
-        # send only if there is a new file
         latest_json_file = get_latest_json_file()
         if latest_json_file != None:
             with open(latest_json_file, 'r') as f:
                 data = json.load(f)
             data = bytes([255]) + bytes([255]) + bytes([18]) + bytes([255]) + bytes([255]) + bytes([12]) + "Battery Data:".encode()+str(data).encode()
             node.send(data)
+            print("New data sent")
         else:
-            print("No new file found")
+            print("No new data to send")
         time.sleep(0.2)
         timer_task = Timer(seconds,send_json_file_continue)
         timer_task.start()
@@ -77,61 +56,5 @@ def send_json_file_continue(continue_or_not = True):
 
 node = sx126x.sx126x(serial_num = "/dev/ttyAMA0",freq=868,addr=34,power=22,rssi=True,air_speed=2400,relay=False)
 
-# comment out if you want to select the sending mode
-#----------------------------------------------------
 timer_task = Timer(seconds,send_json_file_continue)
 timer_task.start()
-#----------------------------------------------------
-
-'''
-try:
-    time.sleep(1)
-    print("Press \033[1;32mEsc\033[0m to exit")
-    print("Press \033[1;32mi\033[0m   to send")
-    print("Press \033[1;32ms\033[0m   to send data every 5 seconds")
-    
-    # it will send rpi cpu temperature every 10 seconds 
-    seconds = 5
-    
-    while True:
-
-        if select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], []):
-            c = sys.stdin.read(1)
-
-            # dectect key Esc
-            if c == '\x1b': break
-            # dectect key i
-            if c == '\x69':
-                send_json_file()
-                #send_deal()
-            # dectect key s
-            if c == '\x73':
-                print("Press \033[1;32mc\033[0m   to exit the send task")
-                timer_task = Timer(seconds,send_json_file_continue)
-                timer_task.start()
-                
-                while True:
-                    if sys.stdin.read(1) == '\x63':
-                        timer_task.cancel()
-                        print('\x1b[1A',end='\r')
-                        print(" "*100)
-                        print('\x1b[1A',end='\r')
-                        break
-
-            sys.stdout.flush()
-            
-        node.receive()
-        
-except:
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
-    # print('\x1b[2A',end='\r')
-    # print(" "*100)
-    # print(" "*100)
-    # print('\x1b[2A',end='\r')
-
-termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
-# print('\x1b[2A',end='\r')
-# print(" "*100)
-# print(" "*100)
-# print('\x1b[2A',end='\r')
-'''
