d6804230be1c1bda7869551e9b4bc1aa30d560a5
Robert Kalmar <git@robert-k.net>
2024-06-20 12:25:05 +0200

    changed CON_STR

--- Lora-Azure-Gateway/user_input.py
+++ Lora-Azure-Gateway/user_input.py
@@ -10,5 +10,6 @@ LED_PIN = 4  # GPIO pin where the LED is connected to
 SWITCH_PIN = 18  # GPIO pin where the reed switch is connected to
 
 # Azure
-IOT_HUB_CON_STR = "HostName=VVCloudDasboard.azure-devices.net;DeviceId=Raspberry2;SharedAccessKey=YO58Wn/LZWCqkTq+Kl8yWU57KXjmPFcYULJhAX3WMIk="  # Paste your Azure IoT Hub connection string
-DEVICE_ID = "Raspberry2"  # Paste your Azure IoT Hub device name
+# IOT_HUB_CON_STR = "HostName=VVCloudDasboard.azure-devices.net;DeviceId=Raspberry2;SharedAccessKey=YO58Wn/LZWCqkTq+Kl8yWU57KXjmPFcYULJhAX3WMIk="  # Paste your Azure IoT Hub connection string
+IOT_HUB_CON_STR = "HostName=VoltageVisionariesWebapp.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=lWRMdTmTACTvPq0tyEWIFCjL9zt3jw4jkAIoTFQEvsE="
+DEVICE_ID = "Raspberry"  # Paste your Azure IoT Hub device name
